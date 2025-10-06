ecd1b3626db4486afa8ecedd3ea1eba919e85020
Suresh-web-dev <dssuresh68@gmail.com>
2025-10-05 19:10:14 -0700

    settings changes

--- contact_book/settings.py
+++ contact_book/settings.py
@@ -15,6 +15,10 @@ import os
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 
+ALLOWED_HOSTS = ["https://contact-vault.onrender.com", "localhost", "127.0.0.1"]
+
+CSRF_TRUSTED_ORIGINS = ["https://contact-vault.onrender.com"]
+
 if not DEBUG:  # production
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
